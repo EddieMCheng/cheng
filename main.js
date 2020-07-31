b9a9f51d25e9e000d993f47aa2e589ca3ea4f535
Eddie Cheng <eddie@catino.se>
2020-07-31 11:31:52 +0200

    enable webrtc peer functionality

--- main.js
+++ main.js
@@ -22,6 +22,7 @@
   function startup() {
     initButton = document.getElementById('initButton');
     connectButton = document.getElementById('connectButton');
+    finalizeButton = document.getElementById('finalizeButton');
     disconnectButton = document.getElementById('disconnectButton');
     sendButton = document.getElementById('sendButton');
     messageInputBox = document.getElementById('message');
@@ -30,32 +31,21 @@
     // Set event listeners for user interface widgets
     initButton.addEventListener('click', initPeers, false);
     connectButton.addEventListener('click', connectPeers, false);
+    finalizeButton.addEventListener('click', finalizePeers, false);
     disconnectButton.addEventListener('click', disconnectPeers, false);
     sendButton.addEventListener('click', sendMessage, false);
-  }
-  
-  // Connect the two peers. Normally you look for and connect to a remote
-  // machine here, but we're just connecting two local objects, so we can
-  // bypass that step.
-  
-  function initPeers() {
-    // Create the local connection and its event listeners
-    
+
     localConnection = new RTCPeerConnection();
-    
-    
-    // Create the data channel and establish its event listeners
+    localConnection.ondatachannel = receiveChannelCallback;
+
     sendChannel = localConnection.createDataChannel("sendChannel");
     sendChannel.onopen = handleSendChannelStatusChange;
     sendChannel.onclose = handleSendChannelStatusChange;
-    
-    // Create the remote connection and its event listeners
-    
-    remoteConnection = new RTCPeerConnection();
-    remoteConnection.ondatachannel = receiveChannelCallback;
-    
-    // Set up the ICE candidates for the two peers
-    
+
+  }
+
+  function initPeers() {
+
     localConnection.onicecandidate = e =>{
       
       if(e.candidate){
@@ -66,14 +56,12 @@
           firstLocalCandidate=true;
         }
         else{
-          localCandidate2Text.value=jsonCandidate;
           firstLocalCandidate=false;
         }
       }
         
     }
 
-    // Now create an offer to connect; this starts the process
     
     localConnection.createOffer()
     .then(offer => localConnection.setLocalDescription(offer))
@@ -81,65 +69,45 @@
         var jsonDesc=JSON.stringify(localConnection.localDescription);
         console.log("offer: "+jsonDesc);
         callerText.value=jsonDesc;
-        
-    
+
     })
     .catch(handleCreateDescriptionError);
   }
   function connectPeers(){
 
-    var desc=new RTCSessionDescription(JSON.parse(callerText.value));
-        remoteConnection.setRemoteDescription(desc);
+    let desc=new RTCSessionDescription(JSON.parse(callerText.value));
+    localConnection.setRemoteDescription(desc);
 
     let candidate = new RTCIceCandidate(JSON.parse(localCandidateText.value));
-    remoteConnection.addIceCandidate(candidate);
-    candidate = new RTCIceCandidate(JSON.parse(localCandidate2Text.value));
-    remoteConnection.addIceCandidate(candidate);
+    localConnection.addIceCandidate(candidate);
 
-
-    remoteConnection.onicecandidate = e =>{
-      if(e.candidate){
-        let jsonCandidate=JSON.stringify(e.candidate);
-        let candidate = new RTCIceCandidate(JSON.parse(jsonCandidate));
-        localConnection.addIceCandidate(candidate);
-        console.log("remote: "+jsonCandidate);
-        if(!firstRemoteCandidate){
-          remoteCandidateText.value=jsonCandidate;
-          firstRemoteCandidate=true;
-        }
-        else{
-          remoteCandidate2Text.value=jsonCandidate;
-          firstRemoteCandidate=false;
-        }
-      }
-    }
-
-    remoteConnection.createAnswer()
-    .then(answer => remoteConnection.setLocalDescription(answer))
-    .then(() =>{ 
-      var jsonDesc=JSON.stringify(remoteConnection.localDescription);
-      console.log("answer: "+jsonDesc);
-      answerText.value=jsonDesc;
-      var desc=new RTCSessionDescription(JSON.parse(answerText.value));
+    localConnection.createAnswer()
+      .then(answer => localConnection.setLocalDescription(answer))
+      .then(() =>{ 
+        var jsonDesc=JSON.stringify(localConnection.localDescription);
+        console.log("answer: "+jsonDesc);
+        answerText.value=jsonDesc;
+        
+        
+    })
+    .catch(handleCreateDescriptionError);
+  }
+  
+  function finalizePeers(){
+    let desc=new RTCSessionDescription(JSON.parse(answerText.value));
       localConnection.setRemoteDescription(desc);
-  })
-  .catch(handleCreateDescriptionError);
   }
     
-  // Handle errors attempting to create a description;
-  // this can happen both when creating an offer and when
-  // creating an answer. In this simple example, we handle
-  // both the same way.
   
   function handleCreateDescriptionError(error) {
     console.log("Unable to create an offer: " + error.toString());
   }
-  
-  // Handle successful addition of the ICE candidate
-  // on the "local" end of the connection.
+
   
   function handleLocalAddCandidateSuccess() {
+    initButton.disabled = true;
     connectButton.disabled = true;
+    finalizeButton.disabled = true;
   }
 
   // Handle successful addition of the ICE candidate
@@ -238,21 +206,7 @@
     // Close the RTCPeerConnections
     
     localConnection.close();
-    remoteConnection.close();
 
-    sendChannel = null;
-    receiveChannel = null;
-    localConnection = null;
-    remoteConnection = null;
-    
-    // Update user interface elements
-    
-    connectButton.disabled = false;
-    disconnectButton.disabled = true;
-    sendButton.disabled = true;
-    
-    messageInputBox.value = "";
-    messageInputBox.disabled = true;
   }
   
   // Set up an event listener which will run the startup
